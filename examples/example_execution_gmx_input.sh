04e934f41be1e01afd6dc0f5054de38ec3b4b15b
Benjamin Ries <benjamin-ries@outlook.com>
2023-09-27 15:19:24 +0200

    Delete examples/example_execution_gmx_input.sh

--- examples/example_execution_gmx_input.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script is an example execution running the example CyclophilinD abfe simulations.
-
-conda activate abfe
-
-ABFE_GMX_CLI -d  ./CyclophilinD_data/gromacs_in/input \
-                    -o ./CyclophilinD_data/gromacs_in/abfe \
-                    -nr 3 \
-                    -submit
