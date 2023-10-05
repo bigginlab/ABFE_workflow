4d56d7c83fcd153a89a7be52d1c5689919e6e010
riesben <benjamin-ries@outlook.com>
2023-10-05 11:14:16 +0200

    adding file endings, including datafiles into the packaging process.

--- src/abfe/rules/__init__.py
+++ src/abfe/rules/__init__.py
@@ -2,11 +2,11 @@ import os
 
 root_path = os.path.dirname(__file__)
 
-ligand_equilibration_workflow_path = root_path + "/ligand_equilibration_workflow/Snakefile"
-ligand_fep_workflow_path = root_path + "/ligand_fep_workflow/Snakefile"
+ligand_equilibration_workflow_path = root_path + "/ligand_equilibration_workflow/Snakefile.smk.smk.smk"
+ligand_fep_workflow_path = root_path + "/ligand_fep_workflow/Snakefile.smk.smk.smk"
 
 ligand_equilibration_gpu_workflow_path = root_path + "/ligand_equilibration_workflow/Snakefile_GPU"
 ligand_fep_gpu_workflow_path = root_path + "/ligand_fep_workflow/Snakefile_GPU"
 
-receptor_workflow_path = root_path + "/receptor_workflow/Snakefile"
-receptor_gmx_workflow_path = root_path + "/receptor_workflow/Snakefile_gmx"
+receptor_workflow_path = root_path + "/receptor_workflow/Snakefile.smk.smk.smk"
+receptor_gmx_workflow_path = root_path + "/receptor_workflow/Snakefile_gmx.smk"
