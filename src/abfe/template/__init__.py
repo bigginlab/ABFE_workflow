7c583dc77a63dd2ba85b83c871d4a0d6875774a3
riesben <benjamin-ries@outlook.com>
2024-02-05 11:15:22 +0100

    adding cluster config file for modifyinng scheduling procedure..

--- src/abfe/template/__init__.py
+++ src/abfe/template/__init__.py
@@ -7,3 +7,7 @@ complex_equil_template_path = root_path + "/complex_equil_workflow"
 complex_fep_template_path = root_path + "/complex_fep_workflow"
 ligand_equil_template_path = root_path + "/ligand_equil_workflow"
 ligand_fep_template_path = root_path + "/ligand_fep_workflow"
+
+
+cluster_config_template_path = root_path + "/cluster_configs"
+default_slurm_config_path = cluster_config_template_path + "/default_slurm_template.json"
