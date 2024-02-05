ce18bfd4967f669d1d881adb76b6efef9147c522
riesben <benjamin-ries@outlook.com>
2024-02-05 11:25:46 +0100

    adding gather command for final or intermediate results gathering.

---
+++ src/abfe_cli/ABFEGatherResults.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import logging
+
+
+loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+for logger in loggers:
+    logger.setLevel(logging.NOTSET)
+
+from abfe.scripts import final_receptor_results
+
+
+def main():
+    # ARGPARSE
+    parser = argparse.ArgumentParser(description="this tool can be used to gather final or temporary results of the ABFE workflow.")
+    parser.add_argument('-i', "--in_dir", help='Input receptor folder, containing ligand approaches', required=True, type=str)
+    parser.add_argument('-o', "--out_dir", help='Output approach folder for free energy .tsvs', required=True, type=str)
+
+    args = parser.parse_args()
+
+    if os.path.isdir(args.in_dir):
+        in_dir = str(args.in_dir)
+    else:
+        raise IOError("could not find in_dir: " + str(args.in_dir))
+    if os.path.isdir(args.out_dir):
+        out_dir = str(args.out_dir)
+    else:
+        raise IOError("could not find out_dir: "+str(args.out_dir))
+
+    print("Trying to gather ready results from ", in_dir)
+    final_receptor_results.get_final_results(out_dir=out_dir, in_root_dir=in_dir)
+
+
+if __name__ == "__main__":
+    main()
