16815c1809670d772f9e05a9f73e7e3817989223
Nithishwer <nithishwer@gamil.com>
2025-12-14 17:11:25 +0000

    chore: Change to make ranvier compatibl

--- src/abfe/template/gmx_submit_kernels/def_gpu_job_cont.sh
+++ src/abfe/template/gmx_submit_kernels/def_gpu_job_cont.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This is a base shell template file
 
+module add GROMACS/2023.1-foss-2022a-CUDA-11.7.0
+
+# to be careful we set the number of OpenMP threads before calling gromacs
 export OMP_NUM_THREADS="${1}"
 STEPNAME="${2}"
 TOPOLOGY="${3}"
@@ -15,6 +18,6 @@ CONFOUT="${STEPNAME}.gro"
 gmx grompp -f ./${STEPNAME}.mdp -c ${STRUCTURE} -r ${STRUCTURE} -p ${TOPOLOGY} -t ${CPT} -o ${GROMACS_TPR} -maxwarn 3
 
 # run gromacs command
-gmx mdrun -ntomp ${OMP_NUM_THREADS} -s $GROMACS_TPR -c $CONFOUT -deffnm ${STEPNAME} -pme gpu -pmefft gpu -bonded gpu -nb gpu
+gmx mdrun -ntomp ${OMP_NUM_THREADS} -s $GROMACS_TPR -c $CONFOUT -deffnm ${STEPNAME} -pme gpu -pmefft gpu -bonded gpu -nb gpu -ntmpi 1
 
 exit 0
